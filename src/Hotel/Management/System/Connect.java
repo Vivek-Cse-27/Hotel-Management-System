0e922435c0787c44f130c8bb7dfb7a71bf37be76
Vivek Barnwal <152686347+Vivek-Cse-27@users.noreply.github.com>
2025-11-16 20:20:02 +0530

    Refactor database connection to use absolute path
    Updated database connection to use dynamic file path.

--- src/Hotel/Management/System/Connect.java
+++ src/Hotel/Management/System/Connect.java
@@ -3,6 +3,7 @@ package Hotel.Management.System;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.io.File;
 
 public class Connect {
     public Connection connection;
@@ -11,12 +12,18 @@ public class Connect {
     public Connect(){
         try {
             Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:C:/Code/HotelMS/Hotel Management System/hotel.db");
+
+            File dbFile = new File("hotel.db");
+            String dbPath = dbFile.getAbsolutePath();
+
+            connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
 
             statement = connection.createStatement();
+
             statement.execute("PRAGMA journal_mode=WAL;");
             statement.execute("PRAGMA synchronous=NORMAL;");
             statement.execute("PRAGMA busy_timeout=5000;");
+
         } catch (Exception e) {
             e.printStackTrace();
         }
